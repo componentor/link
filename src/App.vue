beb570750741d1ce1466813b34f8171630244961
Vue Play <author@vueplay.com>
2025-02-06 16:29:23 +0100

    Stored by Vue Play

--- src/App.vue
+++ src/App.vue
@@ -85,6 +85,7 @@
 	} from 'vue';
 	import Box from '@vueplayio/box';
 	export default {
+		wysiwyg: true,
 		inject: ['theme', 'breakpoint', 'small', 'open', 'forceOpenProvider', 'direction', 'orientation', 'drop', 'level', 'order', 'reverseIcon', 'expand', 'childrenIconSizeProvider', 'childrenCaretProvider', 'childrenCaretSizeProvider', 'model'],
 		provide() {
 			return {
