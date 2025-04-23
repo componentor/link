24a72aa166adebf10d88dff430c7d6a4e925a9a7
Vue Play <author@vueplay.com>
2025-04-23 15:34:24 +0200

    Stored by Vue Play

--- src/App.vue
+++ src/App.vue
@@ -86,7 +86,7 @@
 	} from 'vue';
 	import Box from '@vueplayio/box';
 	export default {
-		inject: ['path', 'pathId', 'pathIdRec', 'setPath', 'theme', 'breakpoint', 'small', 'open', 'forceOpenProvider', 'direction', 'orientation', 'drop', 'level', 'order', 'reverseIcon', 'expand', 'childrenIconSizeProvider', 'childrenCaretProvider', 'childrenCaretSizeProvider', 'model'],
+		inject: ['path', 'pathId', 'setPath', 'theme', 'breakpoint', 'small', 'open', 'forceOpenProvider', 'direction', 'orientation', 'drop', 'level', 'order', 'reverseIcon', 'expand', 'childrenIconSizeProvider', 'childrenCaretProvider', 'childrenCaretSizeProvider', 'model'],
 		provide() {
 			return {
 				setPath(path, id) {
@@ -612,7 +612,6 @@
 			focus: false
 		}),
 		mounted() {
-			console.log(this.$vertical, this.pathId, this.route);
 			document.addEventListener('click', this.handleClickOutside);
 			if (this.$vertical && !this.pathId && this.route && this.$route?.path === this.route && !this.show) {
 				console.log(this.$route?.path, 'is eq 2', this.route);
@@ -839,18 +838,8 @@
 			}
 		},
 		watch: {
-			pathId: {
-				handler(id) {
-					console.log('pathid changed', id, this.title);
-					if (this.currentPathId !== id && this.show) {
-						console.log(this.currentPathId, 'is not', id);
-						this.show = false;
-					}
-				},
-				deep: true
-			},
-			pathIdRec(id) {
-				console.log('pathidrec changed', id, this.title);
+			pathId(id) {
+				console.log('pathid changed', id, this.title);
 				if (this.currentPathId !== id && this.show) {
 					console.log(this.currentPathId, 'is not', id);
 					this.show = false;
