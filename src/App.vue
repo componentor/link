9a9189e1f176be13cc110681a3f43d430ec57852
Vue Play <author@vueplay.com>
2025-04-23 16:33:50 +0200

    Stored by Vue Play

--- src/App.vue
+++ src/App.vue
@@ -614,9 +614,7 @@
 		mounted() {
 			document.addEventListener('click', this.handleClickOutside);
 			if (this.$vertical && !this.pathId && this.route && this.$route?.path === this.route && !this.show) {
-				this.$nextTick(() => {
-					this.toggle(true);
-				});
+				this.toggle(true);
 			}
 		},
 		beforeUnmount() {
